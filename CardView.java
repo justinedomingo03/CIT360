61dc78e9fe45e8777c1e83a51919b27b00b81085
justinedomingo03 <38929690+justinedomingo03@users.noreply.github.com>
2018-06-15 13:50:23 -0600

    Add files via upload

---
+++ CardView.java
@@ -0,0 +1,11 @@
+package MVC;
+
+// This is the View.
+public class CardView {
+	public void printCards(String name, String ability, String duration){
+		System.out.println("Arcanum: " + name);
+		System.out.println("Ability: " + ability);
+		System.out.println("Duration: " + duration);
+		System.out.println();
+	}
+}
